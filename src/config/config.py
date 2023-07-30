8382b005d7332d368cf74c98df96f727b9415b5d
diogenes monegro <diogenes_mj@hotmail.com>
2023-07-30 16:06:15 -0400

    fix: change first wave

--- src/config/config.py
+++ src/config/config.py
@@ -4,7 +4,7 @@ WIDTH = 800
 HEIGHT = 600
 
 WAVES = [
-    { "time": 10 , "enemies": 10 },
+    { "time": 0.1 , "enemies": 10 },
     { "time": 5 , "enemies": 20 },
     { "time": 2 , "enemies": 30 },
     { "time": 3 , "enemies": 40 },
